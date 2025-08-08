f699ed1338452587e62fa002b3b5719d5d9c2c65
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-08-08 17:09:13 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import Categories from "./categories/page";
+// make an index.ts file and export this component from it
+// import { HeroDetails } from "./components/HeroSection/HeroDetails";
+// => import { HeroDetails } from "./components/HeroSection";
 import { HeroDetails } from "./components/HeroSection/HeroDetails";
 export default function Home() {
   return (
